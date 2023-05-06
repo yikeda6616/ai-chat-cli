e030dc4b32b40d13221c3e9341b6ca6dfb4caf5e
Yasushi Ikeda <yikeda6616@gmail.com>
2023-05-06 13:02:14 +0900

    refactor: Use OpenAI API wrapper and add GPT-4 support

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,47 @@
 import axios from "axios";
 import * as dotenv from "dotenv";
 import readline from "readline";
+import { Configuration, OpenAIApi } from "openai";
 
 dotenv.config();
 
 const apiKey = process.env.OPENAI_API_KEY;
+const orgId = process.env.OPENAI_ORGANIZATION;
+const model: "gpt-4" | "gpt-3.5-turbo" = "gpt-4";
 
 if (!apiKey) {
   console.error("APIキーが.envファイルに設定されていません。");
   process.exit(1);
 }
 
+const configuration = new Configuration({
+  organization: orgId,
+  apiKey: apiKey,
+});
+const openai = new OpenAIApi(configuration);
+
+export async function ask(content: string, model: string) {
+  try {
+    const response = await openai.createChatCompletion({
+      model: model,
+      messages: [{ role: "user", content: content }],
+    });
+    return response;
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
 let conversationHistory: string[] = [];
 
 async function chatGPT(prompt: string): Promise<string> {
   try {
-    const fullPrompt = `${conversationHistory.join("\n")}\nUser: ${prompt}\nAI: `;
+    const fullPrompt = `${conversationHistory.join(
+      "\n"
+    )}\nUser: ${prompt}\nAI: `;
 
-    const response = await axios.post(
-      "https://api.openai.com/v1/engines/text-davinci-003/completions",
-      {
-        prompt: fullPrompt,
-        max_tokens: 1024,
-        n: 1,
-        stop: null,
-        temperature: 0.7,
-      },
-      {
-        headers: {
-          "Content-Type": "application/json",
-          "Authorization": `Bearer ${apiKey}`,
-        },
-      }
-    );
-
-    const reply = response.data.choices[0].text.trim();
+    const response = await ask(fullPrompt, model);
+    const reply = response.data.choices[0].message?.content;
     conversationHistory.push(`User: ${prompt}`, `AI: ${reply}`);
 
     return reply;
